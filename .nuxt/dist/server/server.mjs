31dff4ac8bc754fbe84c0959c9db41af6ee031e2
Szuumii <jakub.szumskii@gmail.com>
2024-03-08 22:01:52 +0100

    feat: remove .nuxt dir

--- .nuxt/dist/server/server.mjs
+++
@@ -1,1 +0,0 @@
-export { default } from "file:///Users/jakubszumski/GitRepos/PortfolioPage/node_modules/.pnpm/@nuxt+vite-builder@3.10.3_vue@3.4.21/node_modules/@nuxt/vite-builder/dist/runtime/vite-node.mjs"
